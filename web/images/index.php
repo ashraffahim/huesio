10820b557df088fbe21d4c9704aa352d0f8cff79
ashraffahim <ashraf.fahim75@gmail.com>
2023-09-11 01:23:16 +0400

    htaccess for media

--- web/images/index.php
+++ web/images/index.php
@@ -1,6 +1,7 @@
 <?php
 
-header('Content-type: image/' . pathinfo('../../../huesio_storage/media/' . $_GET['q'], PATHINFO_EXTENSION));
-echo file_get_contents('../../../huesio_storage/media/' . $_GET['q']);
+$bn = explode('/', $_SERVER['REQUEST_URI'])[2];
+header('Content-type: image/' . pathinfo('../../../huesio_storage/media/' . $bn, PATHINFO_EXTENSION));
+echo file_get_contents('../../../huesio_storage/media/' . $bn);
 
 ?>
