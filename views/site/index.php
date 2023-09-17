5a7d61ca1b14920e868523d6d22da0f732ba50ce
ashraffahim <ashraf.fahim75@gmail.com>
2023-09-17 13:35:26 +0400

    Index page padding

--- views/site/index.php
+++ views/site/index.php
@@ -15,23 +15,23 @@ $blogIndex = 0;
             <div class="w-full">
                 <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
                     <h4 class="text-3xl text-center p-3"><?= $blogs[$blogIndex]->title ?></h4>
-                    <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                    <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                     <h6 class="text-base p-3"><?= $blogs[$blogIndex]->description ?></h6>
                     <?php $blogIndex++; ?>
                 </a>
             </div>
             <div class="flex">
-                <div class="w-1/2">
+                <div class="w-1/2 px-1">
                     <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
-                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                         <h4 class="text-xl px-3 pt-3"><?= $blogs[$blogIndex]->title ?></h4>
                         <h6 class="text-base px-3 pb-3"><?= $blogs[$blogIndex]->description ?></h6>
                         <?php $blogIndex++; ?>
                     </a>
                 </div>
-                <div class="w-1/2">
+                <div class="w-1/2 px-1">
                     <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
-                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                         <h4 class="text-xl px-3 pt-3"><?= $blogs[$blogIndex]->title ?></h4>
                         <h6 class="text-base px-3 pb-3"><?= $blogs[$blogIndex]->description ?></h6>
                         <?php $blogIndex++; ?>
@@ -42,17 +42,17 @@ $blogIndex = 0;
 
         <div class="flex w-full lg:w-1/2 md:w-1/2 pl-0 lg:pl-2 md:pl-2">
             <div class="w-full lg:w-1/2 md:w-1/2">
-                <div class="w-full">
+                <div class="w-full px-0 lg:px-1 md:px-1">
                     <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
-                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                         <h4 class="text-xl px-3 pt-3"><?= $blogs[$blogIndex]->title ?></h4>
                         <h6 class="text-base px-3 pb-3"><?= $blogs[$blogIndex]->description ?></h6>
                         <?php $blogIndex++; ?>
                     </a>
                 </div>
-                <div class="w-full">
+                <div class="w-full px-0 lg:px-1 md:px-1">
                     <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
-                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                         <h4 class="text-xl px-3 pt-3"><?= $blogs[$blogIndex]->title ?></h4>
                         <h6 class="text-base px-3 pb-3"><?= $blogs[$blogIndex]->description ?></h6>
                         <?php $blogIndex++; ?>
@@ -60,17 +60,17 @@ $blogIndex = 0;
                 </div>
             </div>
             <div class="w-full lg:w-1/2 md:w-1/2">
-                <div class="w-full">
+                <div class="w-full px-0 lg:px-1 md:px-1">
                     <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
-                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                         <h4 class="text-xl px-3 pt-3"><?= $blogs[$blogIndex]->title ?></h4>
                         <h6 class="text-base px-3 pb-3"><?= $blogs[$blogIndex]->description ?></h6>
                         <?php $blogIndex++; ?>
                     </a>
                 </div>
-                <div class="w-full">
+                <div class="w-full px-0 lg:px-1 md:px-1">
                     <a href="<?= $hrefPrefix . $blogs[$blogIndex]->handle ?>">
-                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>">
+                        <img src="<?= Yii::$app->params['imgCdnBaseUrl'] . $blogs[$blogIndex]->file->uuid . '.' . pathinfo($blogs[$blogIndex]->file->name, PATHINFO_EXTENSION) ?>" alt="<?= $blogs[$blogIndex]->title ?>" loading="lazy">
                         <h4 class="text-xl px-3 pt-3"><?= $blogs[$blogIndex]->title ?></h4>
                         <h6 class="text-base px-3 pb-3"><?= $blogs[$blogIndex]->description ?></h6>
                         <?php $blogIndex++; ?>
