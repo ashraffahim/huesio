6a775d16ea8d748f7c03d4962f0d3cb04ea075d8
ashraffahim <ashraf.fahim75@gmail.com>
2023-09-18 19:09:57 +0400

    Ads on read page

--- views/blogs/read.php
+++ views/blogs/read.php
@@ -33,4 +33,15 @@ if (!is_null($blog->image_id)) {
 
 <div class="blog-read">
     <?= StorageManager::getBlogContent($blog->uuid) ?>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1458829588848469"
+     crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+        style="display:block; text-align:center;"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-1458829588848469"
+        data-ad-slot="9633669284"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </div>
